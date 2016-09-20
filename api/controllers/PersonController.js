1fd3f4ad3f3d11fdae62486282e44ad76f0ab787
rkontham <rkontham@aaxiscommerce.com>
2016-09-20 10:40:42 -0700

    Inital Sails App

---
+++ api/controllers/PersonController.js
@@ -0,0 +1,19 @@
+/**
+ * PersonController
+ *
+ * @description :: Server-side logic for managing people
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	findOne: function (request, response) {
+		Person.find(request.params.id).exec(function (error, persons) {
+			var person = persons[0];
+			person.fullName = person.firstName + ' ' + person.lastName;
+			response.json(person);
+		});
+	}
+	
+};
+
